adfa3a8d0c3a2a9a2b640947b09f656c91b4365a
K-Chinmay <chinmay99v@gmail.com>
2023-08-08 00:25:00 +0530

    server 4

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO, {
+    await mongoose.connect(process.env.MONGO.toString(), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       // useMongoClient: true,
